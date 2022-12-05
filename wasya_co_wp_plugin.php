c8f7ca02cc7c4250bc1353987e40f689ceaa20f9
mac_a2141 <victor@piousbox.com>
2022-12-05 13:37:20 -0500

    cleanup of CardWidget, further. Removed most styles from there.

--- wasya_co_wp_plugin.php
+++ wasya_co_wp_plugin.php
@@ -1,21 +1,33 @@
-<?php
-
+<?
 /**
  * Plugin Name:       Wasya Co Wp Plugin
  * Version:           0.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
-**/
+ **/
 
-if (!defined('ABSPATH')){
-    exit;
+if (!defined('ABSPATH')) {
+  exit;
 }
 
-function register_card_widget( $widgets_manager ) {
+function my_scripts() {
+  if (!function_exists('get_plugin_data')) {
+    require_once(ABSPATH . 'wp-admin/includes/plugin.php');
+  }
+  $plugin_data = get_plugin_data(__FILE__);
 
-	require_once( __DIR__ . '/widgets/card_widget.php' );
-
-	$widgets_manager->register( new \CardWidget() );
-
+  wp_register_style('wasya_co_wp_plugin_style', plugins_url('style.css', __FILE__), false, $plugin_data['Version'], 'all');
+  wp_enqueue_style('wasya_co_wp_plugin_style');
 }
-add_action( 'elementor/widgets/register', 'register_card_widget' );
+add_action( 'wp_enqueue_scripts', 'my_scripts' );
+
+
+
+function register_card_widget($widgets_manager) {
+  require_once(__DIR__ . '/widgets/card_widget.php');
+  $widgets_manager->register(new \CardWidget());
+}
+add_action('elementor/widgets/register', 'register_card_widget');
+
+
+
--- widgets/card_widget.php
+++ widgets/card_widget.php
@@ -3,6 +3,10 @@ if (!defined('ABSPATH')) {
   exit;
 }
 
+/**
+ * 0.0.0 - 202211   - it worked fine
+ * 0.0.1 - 20221205 - removed most styling, typography
+**/
 class CardWidget extends \Elementor\Widget_Base {
 
   public function get_name() {
@@ -52,8 +56,6 @@ class CardWidget extends \Elementor\Widget_Base {
       'label_block' => true,
     ] );
 
-    $this->add_control('hr', [ 'type' => \Elementor\Controls_Manager::DIVIDER, ] );
-
     $this->add_control('divider_icon', [
       'label' => esc_html__('Icon'),
       'type' => \Elementor\Controls_Manager::ICONS,
@@ -63,112 +65,23 @@ class CardWidget extends \Elementor\Widget_Base {
       ],
     ] );
 
-    $this->add_control('divider_width', [
-      'label' => esc_html__('Divider Width'),
-      'type' => \Elementor\Controls_Manager::NUMBER,
-      'placeholder' => '50',
-      'default' => 50,
-    ] );
-
     $this->add_control('description', [
       'label' => esc_html__('Description'),
       'type' => \Elementor\Controls_Manager::WYSIWYG,
       'label_block' => true,
     ] );
 
-    $this->end_controls_section();
-    $this->start_controls_section('section_style', [
-      'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+    $this->add_control('button_link', [
+      'label' => esc_html__('Button Link'),
+      'type' => \Elementor\Controls_Manager::TEXT,
+      'label_block' => true,
     ] );
 
-    $this->add_control('hr', [ 'type' => \Elementor\Controls_Manager::DIVIDER, ] );
-
-    $this->add_control('title_options', [
-      'label' => esc_html__('Title Options'),
-      'type' => \Elementor\Controls_Manager::HEADING,
-      'separator' => 'before',
-    ] );
-
-    $this->add_control('title_alignment', [
-      'type' => \Elementor\Controls_Manager::CHOOSE,
-      'label' => esc_html__('Title Alignment'),
-      'options' => [
-        'left' => [
-          'title' => esc_html__('Left'),
-          'icon' => 'eicon-text-align-left',
-        ],
-        'center' => [
-          'title' => esc_html__('Center'),
-          'icon' => 'eicon-text-align-center',
-        ],
-        'right' => [
-          'title' => esc_html__('Right'),
-          'icon' => 'eicon-text-align-right',
-        ],
-        'justify' => [
-          'title' => esc_html__('Justify'),
-          'icon' => 'eicon-text-align-justify',
-        ],
-      ],
-      'default' => 'left',
-    ] );
-
-    $this->add_control('title_color', [
-      'label' => esc_html__('Title Color'),
-      'type' => \Elementor\Controls_Manager::COLOR,
-      'default' => '#515151',
-      'selectors' => [
-        '{{WRAPPER}} h3' => 'color: {{VALUE}}',
-      ],
-    ] );
-
-    $this->add_group_control(\Elementor\Group_Control_Typography::get_type(), [
-      'name' => 'title_typography',
-      'selector' => '{{WRAPPER}} h3',
-    ] );
-
-    $this->add_control('description_options', [
-      'label' => esc_html__('Description Options'),
-      'type' => \Elementor\Controls_Manager::HEADING,
-      'separator' => 'before',
-    ] );
-
-    $this->add_control('description_alignment', [
-      'type' => \Elementor\Controls_Manager::CHOOSE,
-      'label' => esc_html__('Description Alignment'),
-      'options' => [
-        'left' => [
-          'title' => esc_html__('Left'),
-          'icon' => 'eicon-text-align-left',
-        ],
-        'center' => [
-          'title' => esc_html__('Center'),
-          'icon' => 'eicon-text-align-center',
-        ],
-        'right' => [
-          'title' => esc_html__('Right'),
-          'icon' => 'eicon-text-align-right',
-        ],
-        'justify' => [
-          'title' => esc_html__('Justify'),
-          'icon' => 'eicon-text-align-justify',
-        ],
-      ],
-      'default' => 'left',
-    ] );
-
-    $this->add_control('description_color', [
-      'label' => esc_html__('Description Color'),
-      'type' => \Elementor\Controls_Manager::COLOR,
-      'default' => '#515151',
-      'selectors' => [
-        '{{WRAPPER}} p' => 'color: {{VALUE}}',
-      ],
-    ] );
-
-    $this->add_group_control(\Elementor\Group_Control_Typography::get_type(), [
-      'name' => 'description_typography',
-      'selector' => '{{WRAPPER}} p',
+    $this->add_control('button_text', [
+      'label' => esc_html__('Button Text'),
+      'type' => \Elementor\Controls_Manager::TEXT,
+      'label_block' => true,
+      'default' => 'Read More',
     ] );
 
     $this->end_controls_section();
@@ -176,42 +89,26 @@ class CardWidget extends \Elementor\Widget_Base {
 
   protected function render() {
     $s = $this->get_settings_for_display();
-?>
-    <style>
-      section.align-items-stretch .elementor-widget-CardWidget {
-        height: 100%;
-      }
-      section.align-items-stretch .elementor-widget-container {
-        height: 100%;
-      }
-      .CardWidget {
-        box-shadow: 0 0 8px #888888;
-        padding: 10px;
-        height: 100%;
-      }
-      .CardWidget h3 {
-        font-size: 1.5em;
-      }
-      .CardWidget .description {
-        text-align: <?= $s['description_alignment'] ?>;
-      }
-    </style>
+    ?>
+
     <div class="CardWidget" >
-      <div style="text-align: center;">
+      <div class='W1'>
         <img src="<?= $s['image']['url'] ?>" alt="" />
       </div>
-      <div style="margin-top: 10px;"></div>
-      <h3 style="text-align: <?= $s['title_alignment'] ?>;"><?= $s['title'] ?></h3>
-      <div style="margin-top: 10px;"></div>
-      <div style="text-align: center; align-items: center;">
-        <span style="display: inline-block; width: <?= $s['divider_width'] ?>px; border-bottom: 2px solid #CDCDCD; margin: 5px"></span>
+      <h3><?= $s['title'] ?></h3>
+      <div class='W2'>
+        <span></span>
         <?php \Elementor\Icons_Manager::render_icon($s['divider_icon'], ['aria-hidden' => 'true']); ?>
-        <span style="display: inline-block; width: <?= $s['divider_width'] ?>px; border-bottom: 2px solid #CDCDCD; margin: 5px"></span>
+        <span></span>
       </div>
-      <div style="margin-top: 10px;"></div>
       <div class='description'><?= $s['description'] ?></div>
+      <div class="W3 <?= empty($s['button_link']) ? 'hide' : '' ?>" >
+        <a href="<?= $s['button_link'] ?>" class="elementor-button-link elementor-button" role="button">
+          <span class="elementor-button-text"><?= $s['button_text'] ?></span>
+        </a>
+      </div>
     </div>
-<?php
+    <?
   }
 
 }
